c3315df30e92696da53c7681fe2d30fa804df015
sakshambakshi <sakshambakshi@users.noreply.github.com>
2019-06-05 01:35:10 +0530

    Added build

---
+++ build/precache-manifest.605d19383f710840b80eefe239c189ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e676540ccf6cb2337f9bf05b08ec5e89",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "00b34d53dd583904909c",
+    "url": "/build/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "d25e15df395d896dd0eb",
+    "url": "/build/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "00b34d53dd583904909c",
+    "url": "/build/static/js/2.21307c79.chunk.js"
+  },
+  {
+    "revision": "d25e15df395d896dd0eb",
+    "url": "/build/static/js/main.30e91837.chunk.js"
+  },
+  {
+    "revision": "b4d8ef2cb7791815737b",
+    "url": "/build/static/js/runtime~main.9df7376b.js"
+  }
+]);
